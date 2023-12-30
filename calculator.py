1a45c4fc83e4f5b2efc7248b926726a3cbc65c6f
harshithavaralaxmi <155217027+harshithavaralaxmi@users.noreply.github.com>
2023-12-30 11:28:18 +0530

    calculator program using python

---
+++ calculator.py
@@ -0,0 +1,6 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================================================= RESTART: C:/Users/shiva/OneDrive/Desktop/to-do-list.py ================================================
+>>> 
+======================================= RESTART: C:\Users\shiva\AppData\Local\Programs\Python\Python312\simple1.py ======================================
